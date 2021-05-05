0dfd2d627a2f64f9b4c024f8046f43489dad1cf0
Caian6904 <73233343+Caian6904@users.noreply.github.com>
2021-05-05 15:02:08 +0200

    MapChanges

---
+++ Assets/AnimationStateController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    Animator animator;
+    int IsWalking;
+    int IsJumping;
+    int IsBackwards;
+
+    public Rigidbody Rigidbody;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public CharacterController controller;
+
+    private float lastY;
+    public float FallingThreshold = -10f;
+    public bool IsFalling;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float JumpHeight = 3f;
+
+    Vector3 velocity;
+    bool isGrounded;
+    bool ShiftSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        IsWalking = Animator.StringToHash("IsWalking");
+        IsJumping = Animator.StringToHash("IsJumping");
+        IsBackwards = Animator.StringToHash("IsBackwards");
+
+        lastY = transform.position.y;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        bool IsWalking = animator.GetBool("IsWalking");
+        bool forwardpressed = Input.GetKey("w") && isGrounded;
+        bool IsJumping = Input.GetKey("space") && isGrounded;
+        bool IsBackwards = Input.GetKey("s") && isGrounded;
+        bool IsSprinting = Input.GetKey("left shift") && (Input.GetKey("w")) && isGrounded;
+
+        float distancePerSecondSinceLastFrame = (transform.position.y - lastY) * Time.deltaTime;
+        lastY = transform.position.y;
+        
+        if (Rigidbody.velocity.y < FallingThreshold)
+        {
+            IsFalling = true;
+        }
+        else
+        {
+            IsFalling = false;
+        }
+
+        if (IsFalling && !isGrounded)
+        {
+            animator.SetBool("IsFalling", true);
+        }
+
+        if (!IsFalling)
+        {
+            animator.SetBool("IsFalling", false);
+        }
+
+
+
+
+        if (!IsWalking && forwardpressed)
+        {
+            animator.SetBool("IsWalking", true);
+        }
+
+        if (IsWalking && !forwardpressed)
+        {
+            animator.SetBool("IsWalking", false);
+        }
+
+        if (IsJumping)
+        {
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (!IsJumping)
+        {
+            animator.SetBool("IsJumping", false);
+        }
+
+        if (IsBackwards)
+        {
+            animator.SetBool("IsBackwards", true);
+        }
+
+        if (!IsBackwards)
+        {
+            animator.SetBool("IsBackwards", false);
+        }
+
+        if (IsSprinting)
+        {
+            animator.SetBool("IsSprinting", true);
+        }
+
+        if (!IsSprinting)
+        {
+            animator.SetBool("IsSprinting", false);
+        }
+
+        
+    }    
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float JumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+    bool ShiftSpeed;
+
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        ShiftSpeed = Input.GetKey("left shift") && (Input.GetKey("w"));
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (ShiftSpeed)
+        {
+            speed = 75f;
+        }
+
+        if (!ShiftSpeed)
+        {
+            speed = 12f;
+        }
+    }
+}
